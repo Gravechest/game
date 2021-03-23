dc8d03ebd5768216dca8c7dd46e854352c6ac7a5
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-03-23 22:55:59 +0100

    Update socket.cpp

--- socket.cpp
+++ socket.cpp
@@ -1,27 +1,24 @@
-nclude <iostream>
+#include <iostream>
 #include <WinSock2.h>
 #pragma comment (lib,"ws2_32.lib")
 #include <thread>
-char buf[4], coor[2];
 char yeet;
 char map[100];
+char msg;
 int main() {
 	map[45] = 1;
 	WSADATA data;
 	WORD ver = MAKEWORD(2, 2);
 	WSAStartup(ver, &data);
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-	sockaddr_in adress;
-	adress.sin_addr.S_un.S_addr = 0;
-	adress.sin_port = htons(7778);
-	adress.sin_family = AF_INET;
-	bind(sock, (sockaddr*)&adress, sizeof(adress));
-	listen(sock, SOMAXCONN);
-	sockaddr client;
-	int clientsize = sizeof(client);
-	SOCKET clientsocket = accept(sock, &client, &clientsize);
+	sockaddr_in adres;
+	adres.sin_addr.S_un.S_addr = 167946432;
+	adres.sin_port = htons(7778);
+	adres.sin_family = AF_INET;
+	connect(sock, (sockaddr*)&adres, sizeof(adres));
 	while (true) {
-		recv(clientsocket, buf, 4, 0);
-		std::cout << buf << std::endl;
+		std::cin >> msg;
+		std::cout << &msg;
+		send(sock, &msg, 2, 0);
 	}
 }
